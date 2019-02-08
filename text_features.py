4048fa2b9da57ee0950fff743dde548461d8ab33
Alexis Carrillo <Yagwar@users.noreply.github.com>
2019-02-07 19:24:31 -0500

    Add files via upload

--- text_features.py
+++ text_features.py
@@ -1,62 +1,55 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib
-import matplotlib.pyplot as plt
-import random
-import time
-import matplotlib.animation as animation
-import csv
-
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import PCA
-from sklearn import manifold
-from sklearn.cluster import AgglomerativeClustering
-from scipy.cluster import hierarchy
-from sklearn.cluster import DBSCAN
-from sklearn.metrics.pairwise import pairwise_distances
-# from wordcloud import WordCloud
-from sklearn.feature_extraction import text
 
-from mpl_toolkits.mplot3d import Axes3D
+'''
+Este script toma las frecuencias de las palabras y las suma a los autores para identificar el estilo de escritura.
+Se eliminan las primeras 150 (por ser  en extremo frecuentes, se toman como stopwords)
+Se utilizan las 500 palabras más frecuentes
+'''
 
 blog_data=pd.read_csv("blogtext.csv")
 
 blog_data.drop_duplicates(subset="text",inplace=True)
-blog_data.date = pd.to_datetime(blog_data.date,format="%d,%B,%Y", errors='coerce')#pd.to_datetime(blog_data.date,errors="coerce",infer_datetime_format=True)
+blog_data.date = pd.to_datetime(blog_data.date,format="%d,%B,%Y", errors='coerce')
 print("Blog Data Loaded")
 print(blog_data.shape)
-# blog_data.sample(20)
 
-adit_stpwrds=["urllink","nbsp","ve","ll"]
-stp_wrds = text.ENGLISH_STOP_WORDS.union(adit_stpwrds)
+count_vect = CountVectorizer(max_features=150+500)
+n_grams_counts = count_vect.fit_transform(blog_data["text"])#.toarray()
 
-tfidf_transformer = TfidfVectorizer(stop_words=stp_wrds,max_features=500 )
-n_grams_tfidf = tfidf_transformer.fit_transform(blog_data["text"])
-print("TF-IDF matrix done")
-n_grams_tfidf.shape
+print("TF matrix done")
+n_grams_counts.shape
 
-df_out=pd.DataFrame(blog_data["id"]).join(pd.SparseDataFrame(n_grams_tfidf,
-                                         columns=tfidf_transformer.get_feature_names(),
-                                         index=list(blog_data.index)))#.to_csv("tfidf_pd_texts.csv")
+txt_info= n_grams_counts.toarray().sum(axis=0)
+indices = np.argsort(txt_info)[::-1]
 
-# print("exporting tf idf matrix")
+freq_filt=n_grams_counts[:,indices[150:]]
+print("Filtered TF matrix: ",freq_filt.shape)
+
+df_out=pd.SparseDataFrame(freq_filt,
+                   columns=np.array(count_vect.get_feature_names())[indices[150:]],
+                   index=list(blog_data.index))
+df_out.fillna(0,inplace=True)
+
+# print("exporting TF matrix")
 # try:
-#     df_out.to_csv("df_out.csv")
+#     df_out.to_csv("df_freq_out.csv")
 # except Exception as e:
 #     print("type error: " + str(e))
 
-
-print("Grouping tf-idf data by author")
+    
+print("Grouping TF data by author")
 try:
-    usr_txt_ftr=df_out.groupby(['id'], sort=False).sum()
+    usr_txt_frq=df_out.groupby(blog_data["id"],sort=False).sum()
 except Exception as e:
     print("type error: " + str(e))
 
 print("Exporting data")
 try:
-    usr_txt_ftr.to_csv("usr_txt_ftr.csv")
+    usr_txt_frq.to_csv("usr_txt_wrd_frqs.csv")
 except Exception as e:
     print("type error: " + str(e))
+
+    
+print("Process Finished")
